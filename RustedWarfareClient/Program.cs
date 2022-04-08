1aa01402ca324ebbe335a9f666400ae408c0d768
1dNDN <idndn@inbox.ru>
2022-04-08 16:21:10 +0300

    clean client project

--- RustedWarfareClient/Program.cs
+++ RustedWarfareClient/Program.cs
@@ -8,29 +8,6 @@ namespace RustedWarfareClient
     {
         public static void Main()
         {
-            Init();
-            // Api api = new("127.0.0.1") {
-            //     Nickname = "Бот ебучий"
-            // };
-            // api.Connect();
-            // Console.WriteLine("Connected");
-            //
-            //
-            // api.Disconnect();
-            // Console.WriteLine("Disconnected");
-        }
-
-        private static void Init()
-        {
-            NativeWindowSettings nativeWindowSettings = new()
-            {
-                Size = new Vector2i(800, 600),
-                Title = "RW Client",
-                Flags = ContextFlags.ForwardCompatible,
-            };
-
-            using Vent window = new(GameWindowSettings.Default, nativeWindowSettings);
-            window.Run();
         }
     }
 }
